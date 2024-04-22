23bf8b4bf22598ada45d59045ebd9f3687800229
ibrahiMMMohammad07 <ibrahim.mohammad9247@gmail.com>
2024-04-22 15:28:00 +0530

    Add_Navbar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <div class="container">
+        <ul>
+          <li>
+            <a>HOME</a>
+          </li>
+          <li>
+            <a>PROJECT</a>
+          </li>
+          <li>
+            <a>ABOUT</a>
+          </li>
+          <li>
+            <a>CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
